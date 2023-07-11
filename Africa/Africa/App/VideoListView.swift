0934c8f2fe5dedc11e3f4dc7ded0b3035e0b24c9
Aaron Wells <alwells239@gmail.com>
2023-07-11 12:45:35 -0400

    Video list view (Africa App)

--- Africa/Africa/App/VideoListView.swift
+++ Africa/Africa/App/VideoListView.swift
@@ -8,10 +8,40 @@
 import SwiftUI
 
 struct VideoListView: View {
+    // MARK: - PROPERTIES
+
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+
+    // MARK: - BODY
+
     var body: some View {
-        Text("Videos")
+        NavigationView {
+            List {
+                ForEach(videos) { video in
+                    VideoListItemView(video: video)
+                        .padding(.vertical, 8)
+                } //: LOOP
+            } //: LIST
+            .listStyle(InsetGroupedListStyle())
+            .navigationTitle("Videos")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        // Shuffle videos
+                        videos.shuffle()
+                        hapticImpact.impactOccurred()
+                    } label: {
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                }
+            }
+        } //: NAVIGATION
     }
 }
+
+// MARK: - PREVIEW
 
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
