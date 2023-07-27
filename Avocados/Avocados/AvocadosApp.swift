a39236b9996cc1a9a9e14dbb32ba1d7e910937e2
Aaron Wells <alwells239@gmail.com>
2023-07-27 13:43:16 -0400

    Initial Commit (Avocados App)

---
+++ Avocados/Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Aaron Wells on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppView()
+        }
+    }
+}
